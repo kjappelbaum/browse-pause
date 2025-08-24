5cb2ddac916e69f36d8547f7c16146ecddddfc7b
Kevin Maik Jablonka <mail@kjablonka.com>
2025-08-24 22:10:29 +0200

    Refactor interception to use synchronous webNavigation.onBeforeNavigate
    - Implemented synchronous redirection using webNavigation.onBeforeNavigate and redirectUrl.
    - Stored blocked URLs in memory in background.js for immediate access.
    - Added webRequest permission to manifest.json.
    - Added console.log statements for debugging purposes.

--- background.js
+++ background.js
@@ -1,21 +1,38 @@
 let approvedTabs = {};
+let blockedUrls = [];
+
+function loadBlockedUrls() {
+    chrome.storage.sync.get(['blockedUrls'], (result) => {
+        blockedUrls = result.blockedUrls || [];
+        console.log("Loaded blocked URLs: ", blockedUrls);
+    });
+}
 
 chrome.runtime.onInstalled.addListener(() => {
     // Initialize default settings
-  chrome.storage.sync.get(['blockedUrls', 'images'], (result) => {
-    if (!result.blockedUrls) {
-      chrome.storage.sync.set({
-        blockedUrls: ['facebook.com', 'twitter.com', 'reddit.com'],
-        images: [
-          'https://picsum.photos/800/600?random=1',
-          'https://picsum.photos/800/600?random=2',
-          'https://picsum.photos/800/600?random=3',
-          'https://picsum.photos/800/600?random=4',
-          'https://picsum.photos/800/600?random=5'
+    chrome.storage.sync.get(['blockedUrls', 'images'], (result) => {
+        if (!result.blockedUrls) {
+            chrome.storage.sync.set({
+                blockedUrls: ['facebook.com', 'twitter.com', 'reddit.com'],
+                images: [
+                    'https://picsum.photos/800/600?random=1',
+                    'https://picsum.photos/800/600?random=2',
+                    'https://picsum.photos/800/600?random=3',
+                    'https://picsum.photos/800/600?random=4',
+                    'https://picsum.photos/800/600?random=5'
                 ]
-            });
+            }, loadBlockedUrls);
+        } else {
+            loadBlockedUrls();
         }
     });
+});
+
+// Listen for changes in storage to update blockedUrls
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (namespace === 'sync' && changes.blockedUrls) {
+        loadBlockedUrls();
+    }
 });
 
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
@@ -24,17 +41,16 @@ chrome.webNavigation.onBeforeNavigate.addListener((details) => {
         return;
     }
 
-    chrome.storage.sync.get(['blockedUrls'], (result) => {
-        const blockedUrls = result.blockedUrls || [];
-        const shouldBlock = blockedUrls.some(url => {
-            const cleanUrl = url.replace(/^https?:\/\//, '').replace(/^www\./, '');
-            return details.url.includes(cleanUrl);
-        });
-
-        if (shouldBlock) {
-            chrome.tabs.update(details.tabId, { url: chrome.runtime.getURL('interceptor.html') + '?url=' + encodeURIComponent(details.url) });
-        }
+    const shouldBlock = blockedUrls.some(url => {
+        const cleanUrl = url.replace(/^https?:\/\//, '').replace(/^www\./, '');
+        console.log("Checking URL: " + details.url + " against blocked: " + cleanUrl);
+        return details.url.includes(cleanUrl);
     });
+
+    if (shouldBlock) {
+        console.log("Blocking URL: " + details.url);
+        return { redirectUrl: chrome.runtime.getURL('interceptor.html') + '?url=' + encodeURIComponent(details.url) };
+    }
 }, { url: [{ urlMatches: '<all_urls>' }] });
 
 // Listen for messages from the interceptor script
